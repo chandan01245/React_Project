35ade72e0e69c97d68c240ad9278c21ecba20443
Chandan <chandanc01245@gmail.com>
2025-05-21 10:09:31 +0530

    commiting for merge

--- Frontend/src/Pages/Dashboard.tsx
+++ Frontend/src/Pages/Dashboard.tsx
@@ -1,22 +1,25 @@
+import { Outlet } from "react-router-dom";
+import "../App.css";
 import Header from "../Components/Header";
 import Sidebar from "../Components/Sidebar";
-import "../App.css";
-import { Outlet } from "react-router-dom";
-
 
 function Dashboard() {
   return (
-      <div className="flex h-screen w-screen bg-white text-black transition-colors duration-300 overflow-hidden">
-        <Sidebar />
-        <div className="flex flex-col flex-1 h-full">
-          <Header />
-          <div className="flex-1 overflow-auto p-4">
-            <Outlet />
-          </div>
+    <div className="flex h-screen w-screen bg-white text-black transition-colors duration-300 overflow-hidden">
+      <Sidebar />
+      <div className="flex flex-col flex-1 h-full">
+        <Header />
+        <div className="flex-1 overflow-auto p-4">
+          <Outlet />
+          {localStorage.getItem("user_group") === "admin" ? (
+            <h1 className="text-2xl font-bold">Admin Dashboard</h1>
+          ) : (
+            <h1 className="text-2xl font-bold">User Dashboard</h1>
+          )}
         </div>
       </div>
+    </div>
   );
-};
-
+}
 
 export default Dashboard;
