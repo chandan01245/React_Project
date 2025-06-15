8717268b3850b9dbb4241f34910528a98f62dd5e
jonathan-frazer <jonathanfrazerx146@gmail.com>
2025-06-15 17:38:10 +0000

    Added SSH to change LDIF file on LDAP Server From now, whenever a new user is added. The contents of the LDIF file will be appended to aswell as the Database Update

--- Backend/app.py
+++ Backend/app.py
@@ -307,7 +307,6 @@ def add_user():
 
 		# Step 1: Connect
 		transport = paramiko.Transport((hostname, port))
-		print("Connected")
 		transport.connect(username=username, password=password)
 		sftp = paramiko.SFTPClient.from_transport(transport)
 
@@ -318,17 +317,13 @@ def add_user():
 		except IOError:
 			existing_content = ''  # If file doesn't exist, treat as empty
 
-		print("Existing content:-\n",existing_content)
-
 		# Step 3: Append new content
 		combined_content = existing_content + new_content
 
 		# Step 4: Write it all back (overwrite mode)
 		with sftp.file(remote_path, 'w') as remote_file:
 			remote_file.write(combined_content)
-
-		print("New content:-\n",combined_content)
-
+			
 		# Cleanup
 		sftp.close()
 		transport.close()
